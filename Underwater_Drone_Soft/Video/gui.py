9183fec1f6f3931e39b887531d7bfec4135ffa12
Rando Kübar <randokubargmail.com>
2026-02-20 18:51:51 +0200

    RBG to BGR

--- Underwater_Drone_Soft/Video/gui.py
+++ Underwater_Drone_Soft/Video/gui.py
@@ -184,5 +184,5 @@ class VideoWindow(QMainWindow):
 
         frame_resized = cv2.resize(frame, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
 
-        qt_img = QImage(frame_resized.data, new_w, new_h, 3 * new_w, QImage.Format_RGB888)
+        qt_img = QImage(frame_resized.data, new_w, new_h, 3 * new_w, QImage.Format_BGR888)
         self.video_label.setPixmap(QPixmap.fromImage(qt_img))
