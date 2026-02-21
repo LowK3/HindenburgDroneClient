fbf9d032e87eeb0407961657402d2a30e80ca018
Rando Kübar <randokubargmail.com>
2026-02-21 17:30:05 +0200

    Fix: Buffer error in reverting the frame colours

--- Underwater_Drone_Soft/Video/gui.py
+++ Underwater_Drone_Soft/Video/gui.py
@@ -184,7 +184,7 @@ class VideoWindow(QMainWindow):
 
         frame_resized = cv2.resize(frame, (new_w, new_h), interpolation=cv2.INTER_LINEAR)
 
-        frame_resized = frame_resized[:, :, ::-1]
+        frame_resized = frame_resized[:, :, ::-1].copy()
 
         qt_img = QImage(frame_resized.data, new_w, new_h, 3 * new_w, QImage.Format_RGB888)
         self.video_label.setPixmap(QPixmap.fromImage(qt_img))
